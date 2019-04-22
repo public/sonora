d47ae652d6b737e2ec657f29ac6eb88425896b5b
Alex Stapleton <alexs@prol.etari.at>
2019-04-22 13:42:03 +0100

    Add requests to install_requires

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ REQUIRES_PYTHON = ">=3.6.0"
 VERSION = None
 
 # What packages are required for this module to be executed?
-REQUIRED = ["grpcio"]
+REQUIRED = ["grpcio", "requests"]
 
 # What packages are optional?
 EXTRAS = {
