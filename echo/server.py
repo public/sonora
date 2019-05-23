fd76ab4724407a377c4da30f915d5b456b7f65d3
Alex Stapleton <alex.stapleton@ly.st>
2019-05-23 12:16:36 +0100

    Compile echo bindings

--- echo/server.py
+++ echo/server.py
@@ -5,14 +5,14 @@ import grpc
 
 import grpcWSGI.server
 
-import echo_pb2
-import echo_pb2_grpc
+from echo import echo_pb2
+from echo import echo_pb2_grpc
 
 
 class Echo(echo_pb2_grpc.EchoServiceServicer):
     def Echo(self, request, context):
         return echo_pb2.EchoResponse(message=request.message)
-    
+
     def EchoAbort(self, request, context):
         context.set_code(grpc.StatusCode.ABORTED)
         return echo_pb2.EchoResponse(message=request.message)
