d9f83f0cb1bb44181ea362214ff023fc90f8d11a
Alex Stapleton <alex.stapleton@ly.st>
2019-10-25 10:13:56 +0100

    We don't implement these

--- grpcWSGI/client.py
+++ grpcWSGI/client.py
@@ -55,11 +55,9 @@ class WebChannel:
         )
 
     def stream_unary(self, path, request_serializer, response_deserializer):
-        return None
         raise NotImplementedError()
 
     def stream_stream(self, path, request_serializer, response_deserializer):
-        return None
         raise NotImplementedError()
 
 
